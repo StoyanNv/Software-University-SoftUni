f58c6db8ff98eac19a7500285362163a8b5a1618
StoyanNv <nedkov98@gmail.com>
2017-07-26 14:26:28 +0300

    Add

---
+++ C# OOP Advanced/02. Generics - Exercise/05-06 Generic Count Method/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class StartUp
+{
+    public static void Main()
+    {
+        var n = int.Parse(Console.ReadLine());
+        IList<Box<double>> listOfBoxes = new List<Box<double>>();
+        for (int i = 0; i < n; i++)
+        {
+            var input = double.Parse(Console.ReadLine());
+            var box = new Box<double>(input);
+            listOfBoxes.Add(box);
+        }
+        var element = double.Parse(Console.ReadLine());
+        Console.WriteLine(GetGreaterElementsCount(listOfBoxes, element));
+    }
+
+    public static int GetGreaterElementsCount<T>(IList<Box<T>> listOfBoxes, T element)
+        where T : IComparable
+    {
+        return listOfBoxes.Count(x => x.Input.CompareTo(element) > 0);
+    }
+}
