647c57057910fd826f25243b7eab92c1f39e4ab3
StoyanNv <nedkov98@gmail.com>
2017-07-28 21:08:46 +0300

    Add

---
+++ C# OOP Advanced/03. Lab - Iterators and Comparators/1. Library/BookComparator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+public class BookComparator : IComparer<Book>
+{
+    public int Compare(Book x, Book y)
+    {
+        var titleDiff = x.Title.CompareTo(y.Title);
+
+        if (titleDiff != 0)
+        {
+            return titleDiff;
+        }
+
+        return y.Year.CompareTo(x.Year);
+    }
+}
---
+++ C# OOP Advanced/03. Lab - Iterators and Comparators/1. Library/Library.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+
+public class Library : IEnumerable<Book> 
+{
+    private List<Book> books;
+    public Library(params Book[] books)
+    {
+        this.books = new List<Book>(books);
+        this.books.Sort();
+    }
+
+    public IEnumerator<Book> GetEnumerator()
+    {
+        for (int i = 0; i < books.Count; i++)
+        {
+            yield return books[i];
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
