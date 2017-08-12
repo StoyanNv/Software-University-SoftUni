84b5a69f7fe72e53c64e13af18627848cb28f2f5
StoyanNv <nedkov98@gmail.com>
2017-08-12 14:52:06 +0300

    Add

---
+++ C# OOP Advanced/06. UnitTest - Lab/Skeleton.Tests/AxeTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+[TestFixture]
+class AxeTests
+{
+    private const int AxeAttack = 1;
+    private const int AxeDurability = 1;
+    private const int DummyHealth = 20;
+    private const int DummyXp = 1;
+
+    private Axe axe;
+    private Dummy dummy;
+    [SetUp]
+    public void TestInts()
+    {
+        //Arange
+        this.axe = new Axe(AxeAttack, AxeDurability);
+        this.dummy = new Dummy(DummyHealth, DummyXp);
+    }
+
+    [Test]
+    public void AxeLosesDurabilyAfterAttack()
+    {
+        //Act
+        axe.Attack(dummy);
+        //Assert
+        Assert.AreEqual(0, axe.DurabilityPoints, "Axe Durability doesn't change after attack");
+    }
+
+    [Test]
+    public void AttackWithBrokenAxe()
+    {
+
+        //Act
+        axe.Attack(dummy);
+        //Assert
+        Assert.Throws<InvalidOperationException>(() => axe.Attack(dummy));
+    }
+}
