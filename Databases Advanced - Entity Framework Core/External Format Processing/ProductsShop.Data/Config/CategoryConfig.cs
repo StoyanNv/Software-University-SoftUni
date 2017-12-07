b279ec8f04fd7771f5f1c6cda029649b0bf135ff
StoyanNv <nedkov98@gmail.com>
2017-12-07 18:25:47 +0200

    Add

---
+++ Databases Advanced - Entity Framework Core/External Format Processing/ProductsShop.Data/Config/CategoryConfig.cs
@@ -0,0 +1,18 @@
+﻿namespace ProductsShop.Data.Config
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+    using ProductsShop.Models;
+    class CategoryConfig : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(k => k.Id);
+
+            builder.Property(e => e.Name)
+                .IsRequired(true)
+                .IsUnicode(true)
+                .HasMaxLength(15);
+        }
+    }
+}
---
+++ Databases Advanced - Entity Framework Core/External Format Processing/ProductsShop.Data/ProductsShopContext.cs
@@ -0,0 +1,33 @@
+﻿namespace ProductsShop.Data
+{
+    using System;
+    using Microsoft.EntityFrameworkCore;
+    using ProductsShop.Data.Config;
+    using ProductsShop.Models;
+
+    public class ProductsShopContext : DbContext
+    {
+        public ProductsShopContext() { }
+
+        public ProductsShopContext(DbContextOptions options) : base(options) { }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<CategoryProduct> CategoriesProducts { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(DbConfig.ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserConfig());
+            modelBuilder.ApplyConfiguration(new ProductConfig());
+            modelBuilder.ApplyConfiguration(new CategoryConfig());
+            modelBuilder.ApplyConfiguration(new CategoriesProductsConfig());
+        }
+    }
+}
