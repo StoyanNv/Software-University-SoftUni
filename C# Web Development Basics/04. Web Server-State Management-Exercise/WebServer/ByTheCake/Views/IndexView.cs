eaa73e33cecce26f3b8de9b70c8da9c6a30118dd
StoyanNv <nedkov98@gmail.com>
2018-06-13 00:14:29 +0300

    Add-Web Server-State Management-Exercise

---
+++ C# Web Development Basics/04. Web Server-State Management-Exercise/WebServer/ByTheCake/Views/IndexView.cs
@@ -0,0 +1,19 @@
+﻿namespace HTTPServer.ByTheCake.Views
+{
+    using Server.Contracts;
+
+    public class IndexView : IView
+    {
+        private readonly string htmlFile;
+
+        public IndexView(string htmlFile)
+        {
+            this.htmlFile = htmlFile;
+        }
+
+        public string View()
+        {
+            return this.htmlFile;
+        }
+    }
+}
---
+++ C# Web Development Basics/04. Web Server-State Management-Exercise/WebServer/Launcher.cs
@@ -0,0 +1,25 @@
+﻿using HTTPServer.ByTheCake;
+using HTTPServer.Server;
+using HTTPServer.Server.Routing;
+
+namespace HTTPServer
+{
+    internal class Launcher
+    {
+        private static void Main(string[] args)
+        {
+            Run(args);
+        }
+
+        private static void Run(string[] args)
+        {
+            var mainApplication = new ByTheCakeApplication();
+            var appRouteConfig = new AppRouteConfig();
+            //TODO: Configure App Route Configuration
+            mainApplication.Configure(appRouteConfig);
+            var server = new WebServer(8000, appRouteConfig);
+
+            server.Run();
+        }
+    }
+}
